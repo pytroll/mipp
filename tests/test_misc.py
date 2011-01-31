de7c9305e322b0ee48520e3b6b1bad037f143313
Esben S. Nielsen <esn@dmi.dk>
2011-01-31 14:29:55 +0100

    New calibration uses numexpr when available

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -5,7 +5,7 @@ import cStringIO
 
 import buildpath_to_syspath
 print sys.path
-from xrit import cfg, geosnav
+from xrit import cfg
 
 datadir = os.path.dirname(__file__) + '/data'
 
@@ -33,19 +33,6 @@ class Test(unittest.TestCase):
         fp.close()
         self.assertTrue(text1 == text2, msg='Reading %s.cfg failed'%cfgfile)
 
-    def test_navigation(self):
-        expected = ((57.0, -0.0),
-                    (2500, 2500),
-                    (57.0, 76.504177857169125),
-                    (2500, 100),
-                    (313, 2500))
-        nav = geosnav.GeosNavigation(57.0, 18204444, 18204444, 2500, 2500)
-        values = (nav.lonlat((2500, 2500)),
-                  nav.pixel((57.0, 0)),
-                  nav.lonlat((2500, 100)),               
-                  nav.pixel(nav.lonlat((2500, 100))),
-                  nav.pixel((0,0)))
-        self.assertTrue(values == expected, msg='Error in navigation')
                
 if __name__ == '__main__':
     import nose
