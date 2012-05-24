6722ee43a8984994c760bff22001edfbcd6d6adb
Lars Orum Rasmussen <ras@dmi.dk>
2012-05-24 08:28:20 +0000

    Re-imported low level XRIT readers

--- mipp/xrit/__init__.py
+++ mipp/xrit/__init__.py
@@ -1,3 +1,12 @@
 #
 from mipp.xrit import sat
 from mipp.xrit.mda import Metadata
+
+# low level XRIT data readers.
+from mipp.xrit._xrit import (read_prologue,
+                             read_epilogue,
+                             read_imagedata,
+                             read_gts_message,
+                             read_mpef_clm,
+                             decompress,
+                             list)
