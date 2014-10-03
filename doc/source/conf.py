f64e723cdcd20dad2c00a40626ea2b7c734dff06
Lars Orum Rasmussen <ras@dmi.dk>
2014-10-03 11:29:36 +0000

    Corrected sys.path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,10 +18,7 @@ import math
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('../../'))
-#sys.path.insert(0, os.path.abspath('../../mipp'))
-#sys.path.insert(0, os.path.abspath('../../mipp/xrit'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 class Mock(object):
     def __init__(self, *args, **kwargs):
