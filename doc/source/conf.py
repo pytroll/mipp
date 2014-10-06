91fd6878ba3f64f111129fd07099328a59ecd2f2
Lars Orum Rasmussen <ras@dmi.dk>
2014-10-06 08:32:40 +0000

    Cleaning doc II

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -129,7 +129,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'sphinxdoc'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -230,3 +230,6 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
+
+# Example configuration for intersphinx: refer to the Python standard library.
+##intersphinx_mapping = {'http://docs.python.org/': None}
