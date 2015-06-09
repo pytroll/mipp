6130739f55a80601ba955e380a4aeeda2e390e15
jferencik <ioan.ferencik@geomodel.eu>
2015-06-09 14:59:32 +0200

    replaced tabs with spaces

--- mipp/generic_loader.py
+++ mipp/generic_loader.py
@@ -39,61 +39,63 @@ PPP_CFG_VARNAME = 'PPP_CONFIG_DIR'
 
 class GenericLoader(object):
 
-	""" Generic loader for geostationary satellites
-	.
-	"""
+    """ Generic loader for geostationary satellites
+    .
+    """
 
-	def __init__(self, satid, channels=None, timeslot=None, files=None):
-		""" Locate files and read metadata.
-			There
-		"""
+    def __init__(self, satid, channels=None, timeslot=None, files=None):
+        """ Locate files and read metadata.
+            There
+        """
 
-		self.image = None
-		if files is not None:
-			try:
-				files[0]
-			except TypeError:
-				raise TypeError('Files argument has to be an iterable containing string elements representing full path to HRIT files')
-			self.files = files
+        self.image = None
+        if files is not None:
+            try:
+                files[0]
+            except TypeError:
+                raise TypeError('Files argument has to be an iterable containing string elements representing full path to HRIT files')
+            self.files = files
 
-		else:
+        else:
 
-			if timeslot:
-				# Get list of files from the timeslot
-				#1 read config
-				####
-				if not PPP_CFG_VARNAME in os.environ.keys():
-					raise RuntimeError('Could not find the pytroll config directory environmet variable "%s" ' % (PPP_CFG_VARNAME))
-				if satid is None:
-					raise ValueError('satid argument can not be omitted or be None')
-				#get the mtsat config
-				mtsat_cfg = cfg.read_config(satid)
-				#some confusin exists about the levels in the config file
-				#it seeme level1 corresponds to mipp and level2 corresponds to mpop. when reaodin data from mipp level 1 is used when reading data from
-				#mpop level 2 is used. unde the hood mpop uses mpip so this all ends in mipp
-				data_level = [e for e in mtsat_cfg.sections if 'level1' in e][0]
-				cfg_level = mtsat_cfg(data_level)
+            if timeslot:
+                # Get list of files from the timeslot
+                #1 read config
+                ####
+                if not PPP_CFG_VARNAME in os.environ.keys():
+                    raise RuntimeError('Could not find the pytroll config directory environmet variable "%s" ' % (PPP_CFG_VARNAME))
+                if satid is None:
+                    raise ValueError('satid argument can not be omitted or be None')
+                #get the mtsat config
+                mtsat_cfg = cfg.read_config(satid)
+                #some confusin exists about the levels in the config file
+                #it seeme level1 corresponds to mipp and level2 corresponds to mpop. when reaodin data from mipp level 1 is used when reading data from
+                #mpop level 2 is used. unde the hood mpop uses mpip so this all ends in mipp
+                data_level = [e for e in mtsat_cfg.sections if 'level1' in e][0]
+                cfg_level = mtsat_cfg(data_level)
+                data_dir = cfg_level['dir']
+                filename = cfg_level['filename']
+                print filename
 
 
+                #2 filter the files for this specific date
+                # set the files attribute
 
-	            #2 filter the files for this specific date
-				# set the files attribute
+            else:
+                raise IOError("Either files or timeslot needs to be provided!")
+        self.mda = self._get_metadata()
+    def __getitem__(self, slice):
+        pass
 
-			else:
-				raise IOError("Either files or timeslot needs to be provided!")
-		self.mda = self._get_metadata()
-	def __getitem__(self, slice):
-		pass
+    def load(self, area_extent=None, channels=None, calibrate="1"):
+        self._channels = channels
+        self.mda.area_extent = area_extent
+        return self.__getitem__(self._get_slice_obj())
 
-	def load(self, area_extent=None, channels=None, calibrate="1"):
-		self._channels = channels
-		self.mda.area_extent = area_extent
-		return self.__getitem__(self._get_slice_obj())
+    def _get_slice_obj(self):
+        """ Get code from loader.py
+        """
+        pass
 
-	def _get_slice_obj(self):
-		""" Get code from loader.py
-		"""
-		pass
-
-	def _get_metadata(self):
-		pass
+    def _get_metadata(self):
+        pass
